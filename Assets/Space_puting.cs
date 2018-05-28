92e61bfd6859c84c0c99adf2be41ae5e168911da
DESKTOP-HPULG6C\ohta <ohta@kkaa.co.jp>
2018-05-28 13:31:02 +0900

    Create unity project

---
+++ Assets/Space_puting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Space_puting : MonoBehaviour {
+	
+	public GameObject box;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 touchScreenPosition = Input.mousePosition;
+		touchScreenPosition.x = Mathf.Clamp (touchScreenPosition.x, 0.0f, Screen.width);
+		touchScreenPosition.y = Mathf.Clamp (touchScreenPosition.y, 0.0f, Screen.width);
+
+		Camera gamecamera = Camera.main;
+		Ray touchPointToRay = gamecamera.ScreenPointToRay (touchScreenPosition);
+
+
+
+		
+	}
+}
---
+++ Assets/Spaceput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceput : MonoBehaviour {
+
+	public GameObject box;
+
+	// Use this for initialization
+	void Update ()
+	{
+		
+
+		if (Input.GetKey (KeyCode.Space)) {
+			print ("ooooo");
+			Instantiate (box, new Vector3 (0, 0, 0), Quaternion.identity);
+		}
+	}
+}
+
+			
+			
+       
+		
+
+	
